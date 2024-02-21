2deb172328c2b8a8638564c44120a9079ab886e3
Hayato Takahashi <104160005+Hayato-T-08@users.noreply.github.com>
2024-02-21 08:10:35 +0000

    modify

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
-  def books
-  end
-
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -46,6 +43,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title,:body)
   end
 end
